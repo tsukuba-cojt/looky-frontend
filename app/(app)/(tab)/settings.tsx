b6c6e991c8208e9e10ab17ac414b59824c58dabc
Yushin Ito <yitengyouxin8@gmail.com>
2025-07-02 16:00:54 +0900

    add: 設定のUIを追加

--- app/(app)/(tab)/settings.tsx
+++ app/(app)/(tab)/settings.tsx
@@ -1,27 +1,40 @@
 import { useTranslation } from "react-i18next";
+import { SafeAreaView } from "react-native-safe-area-context";
 import { toast } from "sonner-native";
-import { YStack } from "tamagui";
+import { XStack, YStack } from "tamagui";
 import { Button } from "@/components/Button";
+import { Icons } from "@/components/Icons";
 import { signOut } from "@/lib/auth";
 
 const SettingsPage = () => {
   const { t } = useTranslation("settings");
 
   return (
-    <YStack flex={1} items="center" justify="center">
-      <Button
-        variant="primary"
-        onPress={() => {
-          try {
-            signOut();
-          } catch {
-            toast.error(t("error"));
-          }
-        }}
-      >
-        <Button.Text>Sign Out</Button.Text>
-      </Button>
-    </YStack>
+    <SafeAreaView style={{ flex: 1 }}>
+      <YStack flex={1} pt="$8" px="$8" gap="$8">
+        <Button
+          variant="ghost"
+          onPress={() => {
+            try {
+              signOut();
+            } catch {
+              toast.error(t("error"));
+            }
+          }}
+          justify="space-between"
+        >
+          <XStack gap="$2" items="center">
+            <Button.Icon>
+              <Icons.logout />
+            </Button.Icon>
+            <Button.Text>{t("logout")}</Button.Text>
+          </XStack>
+          <Button.Icon>
+            <Icons.chevronRight />
+          </Button.Icon>
+        </Button>
+      </YStack>
+    </SafeAreaView>
   );
 };
 
