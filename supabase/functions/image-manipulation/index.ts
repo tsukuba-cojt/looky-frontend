4f9483b62a995046d9fd9721c57cdf5a13519659
Yushin Ito <yitengyouxin8@gmail.com>
2025-07-14 03:56:33 +0900

    add: 画像のフォーマットを修正

--- supabase/functions/image-manipulation/index.ts
+++ supabase/functions/image-manipulation/index.ts
@@ -2,6 +2,9 @@ import {
   ImageMagick,
   initializeImageMagick,
 } from "npm:@imagemagick/magick-wasm";
+import { Hono } from "jsr:@hono/hono";
+
+const app = new Hono();
 
 const wasmBytes = await Deno.readFile(
   new URL(
@@ -13,24 +16,34 @@ await initializeImageMagick(
   wasmBytes,
 );
 
-Deno.serve(async (req) => {
-  const formData = await req.formData();
-  const file = formData.get("file") as Blob;
+app.post("/send-email", async (c) => {
+  try {
+    const formData = await c.req.formData();
+    const file = formData.get("file") as Blob;
 
-  const content = new Uint8Array(await file.arrayBuffer());
+    const content = new Uint8Array(await file.arrayBuffer());
 
-  const result = ImageMagick.read(
-    content,
-    (img): Uint8Array => {
-      img.resize(500, 500);
-      return img.write(
-        (data) => data,
-      );
-    },
-  );
+    const result = ImageMagick.read(
+      content,
+      (img): Uint8Array => {
+        img.resize(500, 500);
+        return img.write(
+          (data) => data,
+        );
+      },
+    );
 
-  return new Response(
-    result,
-    { headers: { "Content-Type": "image/png" } },
-  );
+    return new Response(
+      result,
+      { headers: { "Content-Type": "image/png" } },
+    );
+  } catch (error) {
+    console.error(error);
+
+    return new Response(JSON.stringify({ error }), {
+      status: 400,
+    });
+  }
 });
+
+Deno.serve(app.fetch);
