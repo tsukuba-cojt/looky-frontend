bd44ea75f3a84b7d39d21d657fe4297c862194d7
Yushin Ito <yitengyouxin8@gmail.com>
2026-03-18 14:46:49 +0900

    fix: 軽微な修正

--- supabase/functions/send-email/index.ts
+++ supabase/functions/send-email/index.ts
@@ -1,22 +1,27 @@
 import "jsr:@supabase/functions-js/edge-runtime.d.ts";
 import { Hono } from "jsr:@hono/hono";
 import React from "npm:react";
-import { Webhook } from "https://esm.sh/standardwebhooks";
+import { Webhook } from "https://esm.sh/standardwebhooks@1.0.0";
 import { Resend } from "npm:resend";
-import { renderAsync } from "npm:@react-email/components";
+import { render } from "npm:@react-email/render";
 import { MagicLinkEmail } from "./_templates/magic-link.tsx";
 import i18n from "./locales/index.ts";
 
 const app = new Hono();
 
-const resend = new Resend(Deno.env.get("RESEND_API_KEY") as string);
-const hookSecret = Deno.env.get("SEND_EMAIL_HOOK_SECRET") as string;
+const resend = new Resend(Deno.env.get("RESEND_API_KEY") ?? "");
+const hookSecret = (Deno.env.get("SEND_EMAIL_HOOK_SECRET") ?? "").replace(
+  "v1,whsec_",
+  "",
+);
+const supabaseUrl = Deno.env.get("SUPABASE_URL") ?? "";
 
 app.post("/send-email", async (c) => {
   try {
     const payload = await c.req.text();
     const headers = Object.fromEntries(c.req.raw.headers);
     const wh = new Webhook(hookSecret);
+
     const {
       user,
       email_data: { token, token_hash, redirect_to, email_action_type },
@@ -37,9 +42,9 @@ app.post("/send-email", async (c) => {
 
     await i18n.changeLanguage("ja");
 
-    const html = await renderAsync(
+    const html = await render(
       React.createElement(MagicLinkEmail, {
-        supabase_url: Deno.env.get("SUPABASE_URL") ?? "",
+        supabase_url: supabaseUrl,
         token,
         token_hash,
         redirect_to,
@@ -55,19 +60,25 @@ app.post("/send-email", async (c) => {
     });
 
     if (error) {
-      throw error;
+      throw new Error(error.message);
     }
 
-    return new Response(JSON.stringify({}), {
-      status: 200,
-      headers: { "Content-Type": "application/json" },
+    return c.body(JSON.stringify({}), 200, {
+      "Content-Type": "application/json",
     });
-  } catch (error) {
-
-    return new Response(JSON.stringify({ error }), {
-      status: 400,
-      headers: { "Content-Type": "application/json" },
-    });
+  } catch {
+    return c.body(
+      JSON.stringify({
+        error: {
+          http_code: 400,
+          message: "Bad Request",
+        },
+      }),
+      400,
+      {
+        "Content-Type": "application/json",
+      },
+    );
   }
 });
 
